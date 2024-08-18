d6ab01f8b24327521ade5b7a5982909f8a343f4f
Kent Ball <kent.vincent.ball@gmail.com>
2024-08-18 10:32:37 -0400

    Added signup, login, logout, dashboard, post and comment scripts

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const content = document.querySelector('#comment').value.trim();
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (content) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ content, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to add comment.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('#comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
