544656fadb81075aae606011da12040f3731bf78
Kent Ball <kent.vincent.ball@gmail.com>
2024-08-26 11:25:14 -0400

    Render Deploy

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let sequelize;
 
 if (process.env.DATABASE_URL) {
   try {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
+    sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DB_URL, {
       dialect: 'postgres',
       protocol: 'postgres',
       dialectOptions: {
@@ -22,9 +22,9 @@ if (process.env.DATABASE_URL) {
 } else {
   // Local development configuration
   sequelize = new Sequelize(
-    process.env.DB_NAME || 'tech_blog_db',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || 'kentvincent',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
